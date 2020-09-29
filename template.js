7ee92fbf6a7761eff5d1a034737b1e3107c05f98
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-09-29 17:27:56 +0530

    Create template.js

---
+++ template.js
@@ -0,0 +1,15 @@
+export default () => {
+    return `< !doctype html >
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"></meta>
+                <title>MERN Kickstart</title>
+            </head>
+            <body>
+                <div id="root">
+                    <script type="text/javascript" src="/dist/bundle.js">
+                    </script>
+                </div>
+            </body>
+        </html>`
+}
